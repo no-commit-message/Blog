7f91f9dcb8241fea12ce8cbcae1997a824a678a7
guitarhero <ayumu_note@outlook.jp>
2023-09-28 00:00:02 +0900

    Create SignUp

--- MyBlog/MyBlog/urls.py
+++ MyBlog/MyBlog/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from blog.views import top
-from accounts.views import accounts
+from accounts.views import accounts, SignUpView
 
 
 urlpatterns = [
@@ -10,4 +10,5 @@ urlpatterns = [
     path('accounts/', accounts, name='accounts'),
     path('accounts/', include('django.contrib.auth.urls')),
     path('blog/', include('blog.urls')),
+    path('signup/', SignUpView.as_view(), name='signup'),
 ]
--- MyBlog/accounts/views.py
+++ MyBlog/accounts/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.views.generic.edit import CreateView
+from accounts.forms import SignUpForm
+from django.urls import reverse_lazy
 
 # Create your views here.
 @login_required
 def accounts(request):
     return render(request, 'registration/account.html')
+
+class SignUpView(CreateView):
+    form_class = SignUpForm
+    template_name = 'registration/signup.html'
+    success_url = reverse_lazy('login')
